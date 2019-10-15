cf3535b369e9853b1bd520ca70b5ba53ffcd89d0
Carlos Santiago Yanzon <27785807+bizk@users.noreply.github.com>
2019-10-15 11:31:46 -0300

    Initial commit

---
+++ src/main/java/com/api/apiRest/HomeController.java
@@ -0,0 +1,48 @@
+package com.api.apiRest;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import controlador.Controlador;
+import views.EdificioView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		Controlador ctrl = Controlador.getInstancia();
+		List<EdificioView> edificios = ctrl.getEdificiosView();
+		for (EdificioView ev : edificios)
+			logger.info(ev.toString());
+		logger.info("ola mina xd");
+		//System.out.println("hola!");
+		return "home";
+	}
+	
+}
---
+++ src/main/java/com/api/apiRest/Test.java
@@ -0,0 +1,14 @@
+package com.api.apiRest;
+
+import java.util.List;
+
+import controlador.Controlador;
+import views.EdificioView;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Controlador ctrl = Controlador.getInstancia();
+	}
+}
